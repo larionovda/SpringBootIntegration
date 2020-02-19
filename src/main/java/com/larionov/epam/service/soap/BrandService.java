b3d1685f02a7b3cd83d080e0a050d2a61b7fc12f
VincentVega <52175854+larionovda@users.noreply.github.com>
2020-02-19 11:14:19 +0400

    Revert "develop"

--- src/main/java/com/larionov/epam/service/soap/BrandService.java
+++
@@ -1,9 +0,0 @@
-package com.larionov.epam.service.soap;
-
-import generated.Brand;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface BrandService {
-    Brand getBrandByID(Long id);
-}
